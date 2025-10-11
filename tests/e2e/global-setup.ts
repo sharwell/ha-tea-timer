9c12bd3c227ae3290091f471cc4ebaa446fc9843
Sam Harwell <sam@tunnelvisionlabs.com>
2025-10-10 23:36:45 -0500

    fix: include playwright config in ts linting

--- tests/e2e/global-setup.ts
+++ tests/e2e/global-setup.ts
@@ -1,7 +1,7 @@
 import { execSync } from "node:child_process";
 import type { FullConfig } from "@playwright/test";
 
-async function globalSetup(_config: FullConfig) {
+function globalSetup(_config: FullConfig): void {
   if (process.env.PLAYWRIGHT_SKIP_BUILD === "1") {
     return;
   }
